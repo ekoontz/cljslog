d7c6322678d804c6ba781d9603f0119ad3cd00d3
Eugene Koontz <ekoontz@hiro-tan.org>
2019-12-15 11:10:53 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljslog
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/cljslog/core.cljs
@@ -0,0 +1,7 @@
+(ns cljslog.core)
+
+(defn trace [message])
+(defn debug [message])
+(defn info [message])
+(defn warn [message])
+(defn error [message])
