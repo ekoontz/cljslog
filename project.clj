3cc5678148bf85b45ee14ea343bb895437a78d5e
Eugene Koontz <ekoontz@hiro-tan.org>
2019-12-15 20:08:17 +0100

    improve description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljslog "0.1.0-SNAPSHOT"
-  :description "logging for Clojurescript"
+  :description "minimal support for logging for Clojurescript"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
